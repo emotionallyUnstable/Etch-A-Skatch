c2c7c702524d09a21efc0010b2717f6b2042e22e
emotionallyUnstable <95876061+emotionallyUnstable@users.noreply.github.com>
2023-05-18 17:26:54 +0300

    Second commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const colorBtn = document.querySelector("#btn__color");
+const eraserBtn = document.querySelector("#btn__eraser");
+const clearBtn = document.querySelector("#btn__clear");
+
+const gridDisplay = document.querySelectorAll("#grid");
+
+
