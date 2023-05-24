a38833b0ce05705cc8db5ce828530df434dfffdb
emotionallyUnstable <95876061+emotionallyUnstable@users.noreply.github.com>
2023-05-24 08:49:07 +0300

    I don't know what i am doing

--- script.js
+++ script.js
@@ -1,7 +1,61 @@
 const colorBtn = document.querySelector("#btn__color");
 const eraserBtn = document.querySelector("#btn__eraser");
 const clearBtn = document.querySelector("#btn__clear");
+const colorChoise = document.querySelector("#colorPicker");
 
-const gridDisplay = document.querySelectorAll("#grid");
+let gridList = document.querySelector(".grid");
+const gridDisplay = document.querySelectorAll(".grid-element");
+
+
+
+//colorChoise.oninput = (e) => setCurrentColor(e.targer.value);
+
+
+
+//gridList.addEventListener("click", () => {
+    //console.log("Клик!");
+    //gridDisplay.textContent = colorChoise;
+//});
+
+
+
+gridList.addEventListener("mouseenter", (event) => {
+    console.log(event);
+    gridList.classList.add("highlight");
+});
+
+gridList.addEventListener("mouseleave", (event) => {
+    console.log(event);
+    gridList.classList.remove("highlight");
+});
+
+let randomColor = () => {
+    let hexColor = Math.floor(Math.randome() * 16777213).toString(16);
+    return `${hexColor}`;
+};
+console.log(randomColor);
+
+for (const oneCell of gridDisplay) {
+    oneCell.addEventListener("click", (event) => {
+      console.log(event);
+      oneCell.style.backgroundColor = "white";
+    });
+  }
+  const fullPage = document.querySelector("body");
+  fullPage.addEventListener("keydown", (event) => {
+    console.log(event);
+    if (event.key === "b" || event.key === "B") {
+      fullPage.style.backgroundColor = "black";
+    } else {
+      fullPage.style.backgroundColor = "white";
+    }
+  });
+
+
+
+
+
+
+
 
 
